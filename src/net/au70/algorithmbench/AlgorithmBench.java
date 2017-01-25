31461282e2cd2899b2a59544583266a400e31731
Kyle Hinkel <kylehinkel@gmail.com>
2017-01-25 16:53:05 -0500

    Add working gui

--- src/net/au70/algorithmbench/AlgorithmBench.java
+++ src/net/au70/algorithmbench/AlgorithmBench.java
@@ -1,4 +1,16 @@
 package net.au70.algorithmbench;
 
+import net.au70.algorithmbench.gui.Frame;
+
 public class AlgorithmBench {
+
+    Frame frame;
+
+    public AlgorithmBench() {
+        frame = new Frame(500, 500);
+    }
+
+    public static void main(String[] args) {
+        new AlgorithmBench();
+    }
 }
--- src/net/au70/algorithmbench/gui/Frame.java
+++ src/net/au70/algorithmbench/gui/Frame.java
@@ -1,10 +1,25 @@
 package net.au70.algorithmbench.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Frame extends JPanel {
 
-    public Frame() {
+    public Frame(int width, int height) {
+
+        JFrame f = new JFrame();
+        f.getContentPane().add(this);
+        f.setSize(width + 16, height + 40); // +16 and +40 to account for the stuff Windows does. ridiculous
+        f.setTitle("Algorithm Benchmark");
+        f.setVisible(true);
+        f.setFocusable(true);
+        f.setBackground(new Color(0, 0, 0));
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void paint(Graphics gr) {
+        Graphics2D g = (Graphics2D) gr;
+
 
     }
 
