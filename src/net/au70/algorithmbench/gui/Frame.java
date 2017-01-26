965ab09cde7630a1993b9fd4f33ad1dce2043b19
Kyle Hinkel <kylehinkel@gmail.com>
2017-01-26 17:58:35 -0500

    Rework

--- src/net/au70/algorithmbench/gui/Frame.java
+++ src/net/au70/algorithmbench/gui/Frame.java
@@ -1,49 +1,152 @@
 package net.au70.algorithmbench.gui;
 
-import javax.swing.*;
-import java.awt.*;
 
-public class Frame extends JPanel {
+public class Frame extends javax.swing.JFrame {
 
-    public Frame(int width, int height) {
-
-        width += 16; // +16 and +40 to account for the stuff Windows does. ridiculous
-        height += 40;
-
-        JFrame f = new JFrame();
-        f.getContentPane().add(this);
-        f.setSize(width, height);
-        f.setTitle("Algorithm Benchmark");
-        f.setVisible(true);
-        f.setFocusable(true);
-        f.setBackground(new Color(0, 0, 0));
-        f.setLocationRelativeTo(null);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setResizable(false);
+    public Frame() {
+        initComponents();
+    }
 
 
-        // Make the container at the bottom. todo: add this to a new class
-        int containerHeight = 64;
-        int containerWidth = width;
-        JPanel container = new JPanel(new BorderLayout());
-        container.setBackground(new Color(255, 255, 255));
-        container.setSize(containerWidth, containerHeight);
-        container.setLocation(0, height - containerHeight);
-        container.setVisible(true);
-        container.setEnabled(true);
+    private void initComponents() {
 
-        JButton button = new JButton("Test");
-        container.add(button);
+        bottomPanel = new javax.swing.JPanel();
+        startResetButton = new javax.swing.JButton();
+        algorithmComboBox = new javax.swing.JComboBox<>();
+        completionTimeLabel = new javax.swing.JLabel();
+        comparisonsLabel = new javax.swing.JLabel();
+        arrayLabel = new javax.swing.JLabel();
+        timeLabel = new javax.swing.JLabel();
+        canvas = new FrameCanvas();
 
-        f.add(container);
-        
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Algorithm Benchmark");
+        setBackground(new java.awt.Color(255, 255, 255));
+        setResizable(false);
+
+        startResetButton.setText("Start");
+
+        /*
+        startResetButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                startResetButtonActionPerformed(evt);
+            }
+        });
+         */
+        startResetButton.addActionListener(evt -> startResetButtonActionPerformed(evt));
+
+        algorithmComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        completionTimeLabel.setText("Completion time:");
+
+        javax.swing.GroupLayout bottomPanelLayout = new javax.swing.GroupLayout(bottomPanel);
+        bottomPanel.setLayout(bottomPanelLayout);
+        bottomPanelLayout.setHorizontalGroup(
+                bottomPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, bottomPanelLayout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(algorithmComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 209, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(completionTimeLabel)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(startResetButton, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addContainerGap())
+        );
+        bottomPanelLayout.setVerticalGroup(
+                bottomPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(bottomPanelLayout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(bottomPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(startResetButton, javax.swing.GroupLayout.DEFAULT_SIZE, 88, Short.MAX_VALUE)
+                                        .addComponent(algorithmComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(completionTimeLabel))
+                                .addContainerGap())
+        );
+
+        comparisonsLabel.setText("Comparisons:");
+
+        arrayLabel.setText("Array Accesses: ");
+
+        timeLabel.setText("Time Elapsed:");
+
+        canvas.setBackground(new java.awt.Color(51, 51, 51));
+        canvas.setSize(new java.awt.Dimension(600, 500));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(bottomPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(canvas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addComponent(comparisonsLabel)
+                                                .addGap(71, 71, 71)
+                                                .addComponent(arrayLabel)
+                                                .addGap(70, 70, 70)
+                                                .addComponent(timeLabel)))
+                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(arrayLabel)
+                                        .addComponent(timeLabel)
+                                        .addComponent(comparisonsLabel))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(canvas, javax.swing.GroupLayout.PREFERRED_SIZE, 358, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(bottomPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        pack();
+    }
+
+
+    private void startResetButtonActionPerformed(java.awt.event.ActionEvent evt) {
 
     }
 
-    public void paint(Graphics gr) {
-        Graphics2D g = (Graphics2D) gr;
+    public static void start() {
+        // Set the Nimbus look and feel
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Frame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
 
-
+        // Create and display the form
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Frame().setVisible(true);
+            }
+        });
     }
+
+
+    private javax.swing.JComboBox<String> algorithmComboBox;
+    private javax.swing.JLabel arrayLabel;
+    private javax.swing.JPanel bottomPanel;
+    private FrameCanvas canvas;
+    private javax.swing.JLabel comparisonsLabel;
+    private javax.swing.JLabel completionTimeLabel;
+    private javax.swing.JButton startResetButton;
+    private javax.swing.JLabel timeLabel;
+
 
 }
